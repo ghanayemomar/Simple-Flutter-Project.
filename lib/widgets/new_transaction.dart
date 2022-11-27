b88d08bc86b057cf6ef9e42e6240f6d3d3717544
gem00n <o.ghanaiem@student.aaup.edu>
2022-11-27 05:03:45 +0200

    113: Adding Transaction with date

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -19,13 +19,16 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
-    widget.addTx(enteredTitle, enteredAmount);
+    widget.addTx(enteredTitle, enteredAmount, _selectedDate);
     Navigator.of(context).pop();
   }
 
